f9eedd45bcffd3e0a9aca4dc006d4210e75715fb
linbudu599 <linbudu599@gmail.com>
2021-08-05 14:04:23 +0800

    chore: set up git hook

--- project/src/configuration.ts
+++ project/src/configuration.ts
@@ -1,12 +1,10 @@
-import { App, Configuration } from "@midwayjs/decorator";
-import { ILifeCycle } from "@midwayjs/core";
-import { Application } from "egg";
-import { join } from "path";
-import * as oss from "@midwayjs/oss";
+import { App, Configuration } from '@midwayjs/decorator';
+import { ILifeCycle } from '@midwayjs/core';
+import { Application } from 'egg';
+import { join } from 'path';
 
 @Configuration({
-  imports: [oss],
-  importConfigs: [join(__dirname, "./config")],
+  importConfigs: [join(__dirname, './config')],
   conflictCheck: true,
 })
 export class ContainerLifeCycle implements ILifeCycle {
